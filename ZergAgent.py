10a49d237ab905e04620765b5d1babd2de2e5131
Álvaro F. P. P <alvarofepipa@gmail.com>
2021-06-10 18:11:25 -0300

    chore: select a drone

--- ZergAgent.py
+++ ZergAgent.py
@@ -1,10 +1,17 @@
 from pysc2.agents import base_agent
-from pysc2.lib import actions
+from pysc2.lib import actions, units
+import random
 
 
 class ZergAgent(base_agent.BaseAgent):
     def step(self, obs):
         super(ZergAgent, self).step(obs)
 
-        #return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+        drones = [unit for unit in obs.observation.feature_units
+                  if unit.unit_type == units.Zerg.Drone]
+
+        if len(drones) > 0:
+            drone = random.choice(drones)
+            return actions.FUNCTIONS.select_point('select_all_type', (drone.x, drone.y))
+
         return actions.FUNCTIONS.no_op()
