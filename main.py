f535131dbb90ca6f5660a3b488f48afde15d97e1
Álvaro F. P. P <alvarofepipa@gmail.com>
2021-06-12 18:36:04 -0300

    chore: create the action methods

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main(unused_argv):
         while True:
             with sc2_env.SC2Env(
                     map_name="Simple64",
-                    players=[sc2_env.Agent(sc2_env.Race.protoss),
+                    players=[sc2_env.Agent(sc2_env.Race.terran),
                              sc2_env.Bot(sc2_env.Race.protoss,
                                          sc2_env.Difficulty.very_easy)],
                     agent_interface_format=features.AgentInterfaceFormat(
