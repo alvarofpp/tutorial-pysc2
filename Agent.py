7db835cd66ce0931b89087fc53f9cea2d4a962f8
Álvaro F. P. P <alvarofepipa@gmail.com>
2021-06-12 18:24:06 -0300

    chore: create the base agent

---
+++ Agent.py
@@ -0,0 +1,38 @@
+import numpy as np
+from pysc2.agents import base_agent
+from pysc2.lib import features
+
+
+class Agent(base_agent.BaseAgent):
+    actions = ("do_nothing",
+               "harvest_minerals",
+               "build_supply_depot",
+               "build_barracks",
+               "train_marine",
+               "attack")
+
+    def get_my_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.alliance == features.PlayerRelative.SELF]
+
+    def get_enemy_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_my_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.build_progress == 100
+                and unit.alliance == features.PlayerRelative.SELF]
+
+    def get_enemy_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.build_progress == 100
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_distances(self, obs, units, xy):
+        units_xy = [(unit.x, unit.y) for unit in units]
+        return np.linalg.norm(np.array(units_xy) - np.array(xy), axis=1)
